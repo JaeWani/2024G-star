7c8fae8353ba25dd47488cc6358fc8dc46c89ebe
janghyuck0909 <sdh230213@sdh.hs.kr>
2023-11-21 00:48:56 +0900

    DashMonster Script Modify

--- 2024G-Star/Assets/Jaewani/Script/DashMonster.cs
+++ 2024G-Star/Assets/Jaewani/Script/DashMonster.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class DashMonster : Monster
 {
+    [SerializeField]
+    private Transform target;
+    Rigidbody2D rigid;
 
 
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        float dis = Vector3.Distance(transform.position, target.position);
+
+        if (dis <= 10)
+        {
+            Attack();
+        }
     }
+
+
     protected override void Attack()
     {
-                
+
+        Vector3 moveDirection = (target.position - transform.position).normalized;
+
+        rigid.AddForce(moveDirection, ForceMode2D.Impulse);
     }
 }
