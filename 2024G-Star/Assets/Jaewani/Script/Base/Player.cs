b7380e851625872ea0bc92b5a534f4212ef66f9d
JaeWani <104667487+JaeWani@users.noreply.github.com>
2023-12-07 02:57:14 +0900

    Example Asset import, Player Aim & weapon Base

--- 2024G-Star/Assets/Jaewani/Script/Base/Player.cs
+++ 2024G-Star/Assets/Jaewani/Script/Base/Player.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameObject Aim;
+    [SerializeField] private GameObject WeaponObject;
+
     [Header("스탯")]
     [SerializeField] private float MoveSpeed;
 
     [Header("컴포넌트")]
     private Rigidbody2D Rb;
+
+    
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
@@ -17,6 +23,8 @@ public class Player : MonoBehaviour
     protected virtual void Update()
     {
         Move();
+        AimMove();
+        WeaponRotation();
     }
 
     protected virtual void Move()
@@ -25,5 +33,25 @@ public class Player : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         Rb.velocity = new Vector2(x, y).normalized * MoveSpeed;
+    }
+    private void AimMove() 
+    {
+        var mousePos = Input.mousePosition;
+        var pos = GameManager.instance.mainCamera.ScreenToWorldPoint(mousePos);
+        Aim.transform.position = new Vector3(pos.x, pos.y,0);
+    }
+    private void WeaponRotation() 
+    {
+        var weaponTransform = WeaponObject.transform;
+        var mousePos = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        float y = mousePos.y - weaponTransform.position.y;
+        float x = mousePos.x - weaponTransform.position.x;
+
+        float angle = Mathf.Atan2(y,x) * Mathf.Rad2Deg;
+
+        weaponTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (angle < 90 && angle > -90) WeaponObject.GetComponent<SpriteRenderer>().flipY = false;
+        else WeaponObject.GetComponent<SpriteRenderer>().flipY = true;
     }
 }
--- 2024G-Star/Assets/Jaewani/Script/Manager/GameManager.cs
+++ 2024G-Star/Assets/Jaewani/Script/Manager/GameManager.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum PlayerState
+    {
+        Playing,
+        Inventory
+    }
+
     public static GameManager instance;
 
+    [Header("컴포넌트")]
     public GameObject Player;
+
+    public Camera mainCamera;
+
+    [Header("상태")]
+    [SerializeField] private PlayerState currentPlayerState;
+    public PlayerState CurrentPlayerState
+    {
+        get => currentPlayerState; 
+        set
+        {
+            currentPlayerState = value;
+            SetMousePointerVisible();
+        }
+    }
+
 
     private void Awake()
     {
         if (instance == null) instance = this;
         else Destroy(gameObject);
+
+
     }
     private void Start()
     {
+        mainCamera = Camera.main;
 
+        SetMousePointerVisible();
     }
 
     private void Update()
     {
 
+    }
+
+    private void SetMousePointerVisible() 
+    {
+        if (currentPlayerState == PlayerState.Playing) Cursor.visible = false;
+        else if (currentPlayerState == PlayerState.Inventory) Cursor.visible = true;
     }
 }
